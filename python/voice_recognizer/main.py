714621a248001a2bcb449bf22b54d00707141e7d
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-14 17:09:35 -0300

    Tweaks

--- python/voice_recognizer/main.py
+++ python/voice_recognizer/main.py
@@ -1,8 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
 
-def text():
-    r = sr.Recognizer()
 # Theoretically this will speak what has been said
 # def SpeakText(command):
 
@@ -10,18 +8,20 @@ def text():
 #     engine.say(command)
 #     engine.runAndWait()
 
+def text():
+    r = sr.Recognizer()
+    print('Listening...')
+
     with sr.Microphone() as source2:
         r.adjust_for_ambient_noise(source2, duration=0.2)
 
         audio2 = r.listen(source2)
 
         spoken_text = r.recognize_google(audio2)
+
         spoken_text = spoken_text.lower()
     # SpeakText(MyText)
     return spoken_text
 
 
-# statement = f"You said:\n{'*'*50}\n {MyText} \n{'*'*50}"
-
-
-print(text())
+print(f"You said: \n{'*'*50}\n {text()} \n{'*'*50}")
