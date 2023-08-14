0dee47838a0078b19b1b74188894768beccf990c
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-14 17:53:53 -0300

    Added a file handler

--- python/voice_recognizer/main.py
+++ python/voice_recognizer/main.py
@@ -1,11 +1,7 @@
 import speech_recognition as sr
-import pyttsx3
-# Theoretically this will speak what has been said
-# def SpeakText(command):
+# import pyttsx3
 
-#     engine = pyttsx3.init()
-#     engine.say(command)
-#     engine.runAndWait()
+f = open('logs.txt', 'w')
 
 def text():
     r = sr.Recognizer()
@@ -15,7 +11,6 @@ def text():
         r.adjust_for_ambient_noise(source2, duration=0.2)
 
         audio2 = r.listen(source2)
-
         spoken_text = r.recognize_google(audio2)
 
         spoken_text = spoken_text.lower()
@@ -23,18 +18,24 @@ def text():
     return spoken_text
 
 
-
-
 trigger = int(input("""Enter 1 to run once\nEnter 2 to run indefinitely\n0 to quit\n"""))
 
 if trigger == 1:
+    # Runs text function
     said_text = text()
     print(f"You said: \n{'*'*50}\n {said_text} \n{'*'*50}")
+    f.write(str(said_text))
     trigger = int(input("""Enter 1 to run once\nEnter 2 to run indefinitely\n0 to quit\n"""))
+
 elif trigger == 2:
+    # Not working great still
     while trigger == 2:
         said_text = text()
         print(f"You said: \n{'*'*50}\n {said_text} \n{'*'*50}")
+        f.write(said_text)
+        f.close
+
 else:
-    print('Stoppting:')
+    print('Stopping:')
+    f.close()
     quit()
