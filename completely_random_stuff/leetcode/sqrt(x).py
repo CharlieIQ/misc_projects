4be1114e38fcf4faff5ea409f5d4298eebce66f8
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-15 11:49:29 -0300

    Started Calculator Project

--- completely_random_stuff/leetcode/sqrt(x).py
+++ completely_random_stuff/leetcode/sqrt(x).py
@@ -5,13 +5,14 @@ class Solution(object):
         :rtype: int
         """
         for num in range(0, int(x/2)+1):
+            print(num)
             if num * num == x:
                 return num
             
         
 
-square_root1 = Solution().mySqrt(9)
+# square_root1 = Solution().mySqrt(9)
 square_root2 = Solution().mySqrt(8)
 
-print(square_root1)
+# print(square_root1)
 print(square_root2)
