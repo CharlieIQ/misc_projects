bd56ea058b35a5df072afc517b622d0d2622d4d2
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-15 14:28:43 -0300

    Still not working 💀

--- javascript/charlie_calculator/script.js
+++ javascript/charlie_calculator/script.js
@@ -1,13 +1,15 @@
-const add_button = document.querySelector('.add');
-const subtract_button = document.querySelector('.subtract');
-const multiply_button = document.querySelector('.multiply');
-const divide_button = document.querySelector('.divide');
+const add_button = document.getElementsByClassName('addButton');
+const subtract_button = document.getElementsByClassName('subtractButton');
+const multiply_button = document.getElementsByClassName('multiplyButton');
+const divide_button = document.getElementsByClassName('divideButton');
 // const equalsCalculate = document.getElementById('equals');
 
-let number1 = Number(document.querySelector('.numberInputOne').value);
-let number2 = Number(document.querySelector('.numberInputTwo').value);
+document.getElementsByClassName('calcHeader').textContent = "cum";
+let number1 = Number(document.getElementsByClassName('numberInputOne').value);
+let number2 = Number(document.getElementsByClassName('numberInputTwo').value);
 
-add_button.addEventListener('click', function () {
+add_button.addEventListener('click', function (e) {
+    e.preventDefault()
     let sum = number1 + number2;
-    document.querySelector('.result').textContent = sum;
+    document.getElementsByClassName('result').textContent = sum;
 });
