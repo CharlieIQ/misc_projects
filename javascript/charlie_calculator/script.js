6a8f62c39ba43c9349423b57047188123362e3bb
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-17 16:12:18 -0300

    Not working still

--- javascript/charlie_calculator/script.js
+++ javascript/charlie_calculator/script.js
@@ -1,17 +1,17 @@
-const add_button = document.getElementsByClassName('addButton');
+const add_button = document.getElementsByClassName('addButton')[0];
 const subtract_button = document.getElementsByClassName('subtractButton');
 const multiply_button = document.getElementsByClassName('multiplyButton');
 const divide_button = document.getElementsByClassName('divideButton');
 // const equalsCalculate = document.getElementById('equals');
 
-const header = document.querySelector('h1')
-header.textContent = "test please work!!!";
+const pageHeader = document.getElementsByClassName('.calcHeader')
+pageHeader.innerText = "test please work!!!";
 
 let number1 = Number(document.getElementsByClassName('numberInputOne').value);
 let number2 = Number(document.getElementsByClassName('numberInputTwo').value);
 
-add_button.addEventListener('click', function (e) {
-    e.preventDefault()
+add_button.addEventListener('click', function () {
     let sum = number1 + number2;
     document.getElementsByClassName('result').textContent = sum;
 });
+
