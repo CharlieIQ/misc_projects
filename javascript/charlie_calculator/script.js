e8450930842b28811122b175473becf0ee93b1c7
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-17 23:02:46 -0300

    Fixed all of the alignment

--- javascript/charlie_calculator/script.js
+++ javascript/charlie_calculator/script.js
@@ -1,15 +1,16 @@
+/*
+I made this as a personal project to work on when I am bored
+*/
+
+// Variables for all of the buttons on page
 const add_button = document.querySelector('.addbutton');
 const subtract_button = document.querySelector('.subtractbutton');
 const multiply_button = document.querySelector('.multiplybutton');
 const divide_button = document.querySelector('.dividebutton');
 const exponent_button = document.querySelector('.exponentbutton')
-// const equalsCalculate = document.getElementById('equals');
+const modulus_button = document.querySelector('.modulusbutton')
 
-// I was having issues with DOM and this is to test fixing it
-const pageHeader = document.querySelector('.calcheader')
-pageHeader.textContent = "Charlie Calculator";
-
-// For adding numbers
+// For adding button
 add_button.addEventListener('click', function (e) {
     // Gets the numbers inputted
     let number1 = Number(document.querySelector('.numberinputone').value);
@@ -62,4 +63,15 @@ exponent_button.addEventListener('click', function (e) {
     e.preventDefault() // Prevents refresh
     let exponent = number1 ** number2; // Raises numebr one to the power of number 2
     document.querySelector('.result').textContent = exponent;
+});
+
+// For modulus button
+modulus_button.addEventListener('click', function (e) {
+    // Gets the numbers inputted
+    let number1 = Number(document.querySelector('.numberinputone').value);
+    let number2 = Number(document.querySelector('.numberinputtwo').value);
+
+    e.preventDefault() // Prevents refresh
+    let modulus = number1 % number2; // Perfomns modulus operation
+    document.querySelector('.result').textContent = modulus;
 });
