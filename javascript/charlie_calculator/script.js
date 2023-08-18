a7e2ccb7a21bd5cced6574be138575515a82eff7
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-17 22:34:08 -0300

    Added exponent button to calculator

--- javascript/charlie_calculator/script.js
+++ javascript/charlie_calculator/script.js
@@ -2,6 +2,7 @@ const add_button = document.querySelector('.addbutton');
 const subtract_button = document.querySelector('.subtractbutton');
 const multiply_button = document.querySelector('.multiplybutton');
 const divide_button = document.querySelector('.dividebutton');
+const exponent_button = document.querySelector('.exponentbutton')
 // const equalsCalculate = document.getElementById('equals');
 
 // I was having issues with DOM and this is to test fixing it
@@ -50,4 +51,15 @@ divide_button.addEventListener('click', function (e) {
     e.preventDefault() // Prevents refresh
     let quotient = number1 / number2; // Divides the numbers
     document.querySelector('.result').textContent = quotient;
+});
+
+// For exponent button
+exponent_button.addEventListener('click', function (e) {
+    // Gets the numbers inputted
+    let number1 = Number(document.querySelector('.numberinputone').value);
+    let number2 = Number(document.querySelector('.numberinputtwo').value);
+
+    e.preventDefault() // Prevents refresh
+    let exponent = number1 ** number2; // Raises numebr one to the power of number 2
+    document.querySelector('.result').textContent = exponent;
 });
