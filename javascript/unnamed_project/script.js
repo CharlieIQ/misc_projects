7f907cd7531a578c7ba35b144ee70bd4a717dd1c
CharlieIQ <charliermclaughlin@gmail.com>
2023-08-30 11:56:11 -0300

    Added a graph

--- javascript/unnamed_project/script.js
+++ javascript/unnamed_project/script.js
@@ -44,29 +44,3 @@ for (let index = 0; index < blockNodelist.length; index++){
         }
 })}
     
-/* TEST FOR A GRAPH
-const xValues = [];
-const yValues = [];
-generateData("x ** 2 + 7", 0, 10, 0.5);
-
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      pointRadius: 1,
-      borderColor: "rgba(255,0,0,0.5)",
-      data: yValues
-    }]
-  },
-  options: {...}
-});
-
-function generateData(value, i1, i2, step = 1) {
-  for (let x = i1; x <= i2; x += step) {
-    yValues.push(eval(value));
-    xValues.push(x);
-  }
-}
-*/
