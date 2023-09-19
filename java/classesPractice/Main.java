385fc21c8e5e115eadb4febebd6c8e8b2d796193
CharlieIQ <charliermclaughlin@gmail.com>
2023-09-19 17:21:38 -0230

    OOP tests

---
+++ java/classesPractice/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Second myCar = new Second();   // Create a myCar object
+        myCar.fullThrottle();      // Call the fullThrottle() method
+        myCar.speed(200);
+    }
+}
