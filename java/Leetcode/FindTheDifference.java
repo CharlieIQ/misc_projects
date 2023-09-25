4dcd791d6db7aa1006aefc91fcbc5cd3e6ac8140
CharlieIQ <charliermclaughlin@gmail.com>
2023-09-25 12:16:18 -0230

    find the diff leetcode

--- java/Leetcode/FindTheDifference.java
+++ java/Leetcode/FindTheDifference.java
@@ -1,5 +1,56 @@
 public class FindTheDifference {
     public static void main(String[] args){
-        
+        String s = "abcd";
+        String t = "abcde";
+        // Output should be e
+        System.out.println(findDifference(s, t));
     }
+
+    public static char findDifference(String s, String t){
+        int originalLen = s.length();
+        int newLen = originalLen + 1;
+        Character[] originalString = new Character[originalLen];
+        Character[] newString = new Character[newLen];
+
+        // Add original string
+        for (int i = 0; i < originalLen; i++){
+            originalString[i] = s.charAt(i);
+        }
+        // For shortened string
+        for (int i = 0; i < originalLen; i++){
+            newString[i] = t.charAt(i);
+        }
+        // For deletion
+        int indexOfDeletedElement = 0;
+        for (int i = newLen; i > 0; i--){
+            for (int j = i; j < newString.length; j++){
+                if (originalString[i].equals(newString[j])){
+                    newString[j] = null;
+                    indexOfDeletedElement = i;
+                }
+            }
+            newString = shortenArray(orderArray(originalString, indexOfDeletedElement));
+        }
+
+        return newString[0];
+    }
+
+    public static Character[] orderArray(Character[] testArray, int indexOfDeletion){
+
+        for (int i = indexOfDeletion; i < testArray.length - 1; i++){
+            testArray[i] = testArray[i+1];
+        }
+
+        return testArray;
+    }
+
+    public static Character[] shortenArray(Character[] testArray){
+        Character[] shortenedArray = new Character[testArray.length - 1];
+
+        for (int i = 0; i < shortenedArray.length; i++){
+            shortenedArray[i] = testArray[i];
+        }
+        return shortenedArray;
+    }
+
 }
