c9e625c276e6787f033084455702235b1564c1b8
CharlieIQ <charliermclaughlin@gmail.com>
2023-11-17 00:22:19 -0400

    Add Binary

--- java/Leetcode/AddBinaryTwo.java
+++ java/Leetcode/AddBinaryTwo.java
@@ -1,11 +1,16 @@
 public class AddBinaryTwo {
-    public static void main(String args){
-
+    public static void main(String[] args){
+        System.out.println(addBinary("1111", "00110"));
     }
 
-
     public static String addBinary(String a, String b) {
-        return intToBinary(convertToInt(a) + convertToInt(b));
+        int length = Math.max(a.length(), b.length());
+
+        if (a.charAt(0) =='1' && b.charAt(0) == '1'){
+            length++;
+        }
+
+        return intToBinary(convertToInt(a) + convertToInt(b) + 1, length);
     }
 
     public static int convertToInt(String binaryString){
@@ -19,9 +24,17 @@ public class AddBinaryTwo {
         return newInt;
     }
 
-    public static String intToBinary(int num){
+    public static String intToBinary(int num, int length){
         String binary = "";
-
+        for (int i = length - 1; i >= 0; i--){
+            if (num - Math.pow(2, i) > 0){
+                num -= Math.pow(2, i);
+                binary += "1";
+            }
+            else{
+                binary += "0";
+            }
+        }
         return binary;
     }
 
