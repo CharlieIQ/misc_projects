7b78de94ab426df356e1b407c85b39952a24cc2b
CharlieIQ <charliermclaughlin@gmail.com>
2024-09-26 17:31:47 -0300

    Prime factors

---
+++ java/1315Module5/FindPrimeFactors.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class FindPrimeFactors {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> factors = new ArrayList<>();
+
+        int num = in.nextInt();
+
+    }
+}
